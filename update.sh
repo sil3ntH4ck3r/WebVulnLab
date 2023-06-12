3c45dd0883d6d4b4f16be651cf9ea5370fa4e7e1
sil3nth4ck3r <93217304+sil3ntH4ck3r@users.noreply.github.com>
2023-06-12 13:21:22 +0200

    Mejorando update.sh

--- update.sh
+++ update.sh
@@ -1,18 +1,66 @@
 #!/bin/bash
 
+# Funciones para imprimir mensajes con colores
+print_info() {
+    printf "\e[1;34m[*] $1\e[0m\n"
+}
+
+print_success() {
+    printf "\e[1;32m[+] $1\e[0m\n"
+}
+
+print_warning() {
+    printf "\e[1;33m[!] $1\e[0m\n"
+}
+
+print_error() {
+    printf "\e[1;31m[-] $1\e[0m\n"
+}
+
+# Obtiene la ruta del directorio actual
+PROJECT_DIR="$(dirname "$(readlink -f "$0")")"
+
+# Cambia al directorio del proyecto
+if cd "$PROJECT_DIR"; then
+    print_info "Cambiando al directorio del proyecto: $PROJECT_DIR"
+else
+    print_error "No se pudo cambiar al directorio del proyecto: $PROJECT_DIR"
+    exit 1
+fi
+
 # Asegúrate de que estás en la rama 'dev'
-git checkout dev
+if git checkout dev > /dev/null 2>&1; then
+    print_info "Cambiando a la rama 'dev'"
+else
+    print_error "No se pudo cambiar a la rama 'dev'"
+    exit 1
+fi
 
 # Obtiene los cambios más recientes del repositorio remoto
-git fetch origin
+if git fetch origin > /dev/null 2>&1; then
+    print_info "Obteniendo cambios del repositorio remoto"
+else
+    print_error "No se pudieron obtener los cambios del repositorio remoto"
+    exit 1
+fi
 
 # Comprueba si hay actualizaciones disponibles
 UPDATES_AVAILABLE=$(git log HEAD..origin/dev --oneline)
 
 if [ -z "$UPDATES_AVAILABLE" ]; then
-    echo "No hay actualizaciones disponibles."
+    print_warning "No hay actualizaciones disponibles."
 else
-    echo "Actualizaciones disponibles. Actualizando..."
-    git merge origin/dev
-    echo "Actualización completada."
+    print_info "Actualizaciones disponibles. ¿Deseas instalarlas? (s/n)"
+    read -r install_choice
+    if [[ $install_choice == "s" ]]; then
+        print_info "Actualizando..."
+        if git merge origin/dev > /dev/null 2>&1; then
+            print_success "Actualización completada."
+        else
+            print_error "No se pudo completar la actualización."
+            exit 1
+        fi
+    else
+        print_info "No se realizará la instalación de la actualización."
+    fi
 fi
